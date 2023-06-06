fedafb3f329f0095d6b667fd02bd681bad6e0fb3
azkifairuz <azkiajmal@gmail.com>
2023-06-06 23:56:12 +0700

    create response update when name null or undefined

--- src/handler.js
+++ src/handler.js
@@ -148,15 +148,41 @@ const editBookHandler = (request, h) => {
   } = request.payload;
   const updatedAt = new Date().toISOString();
 
-  const index = books.findIndex((note) => note.id === bookId);
+  const index = books.findIndex((book) => book.id === bookId);
 
-  const response = h.response({
-    status: 'success',
-    message: 'Buku berhasil diperbarui',
-  });
-  response.code(200);
-  return response;
-}
+  // response when id exixst
+  if (index !== -1) {
+    books[index] = {
+      ...books[index],
+      name,
+      year,
+      author,
+      summary,
+      publisher,
+      pageCount,
+      readPage,
+      reading,
+      updatedAt,
+    };
+
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil diperbarui',
+    });
+    response.code(200);
+    return response;
+  }
+
+  // response when name null
+  if (!name) {
+    const response = h.response({
+      status: 'fail',
+      message: 'Gagal memperbarui buku. Mohon isi nama buku',
+    });
+    response.code(400);
+    return response;
+  }
+};
 module.exports = {
   addBookHandler,
   getAllBookHandler,
